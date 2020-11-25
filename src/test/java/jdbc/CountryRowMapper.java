273e428613f2d31f53b447fd5abf870441f12415
ch <mkmsacc@outlook.com>
2020-11-25 14:54:47 +0700

    Add CountryRowMapper

---
+++ src/test/java/jdbc/CountryRowMapper.java
@@ -0,0 +1,18 @@
+package jdbc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class CountryRowMapper implements RowMapper<Country> {
+	public static final String NAME = "name";
+	public static final String CODE_NAME = "code_name";
+
+	@Override
+	public Country mapRow(ResultSet resultSet, int i) throws SQLException {
+		Country country = new Country(resultSet.getString(NAME),
+				resultSet.getString(CODE_NAME));
+		return country;
+	}
+}
