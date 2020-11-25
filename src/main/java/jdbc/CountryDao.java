7fe43c2121fc57723a0c09bdf8d02d8f7e8442ac
ch <mkmsacc@outlook.com>
2020-11-25 13:41:32 +0700

    First commit

---
+++ src/main/java/jdbc/CountryDao.java
@@ -0,0 +1,60 @@
+package jdbc;
+
+import static jdbc.ConnectionManager.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountryDao {
+	private static final String GET_ALL_COUNTRIES_SQL = "select * from country";
+	private static final String GET_COUNTRIES_BY_NAME_SQL = "select * from country where name like ?";
+
+	public List<Country> getCountryList() {
+		List<Country> countryList = new ArrayList<>();
+
+		try {
+			Connection connection = openConnection();
+			PreparedStatement statement = connection
+					.prepareStatement(GET_ALL_COUNTRIES_SQL);
+			ResultSet resultSet = statement.executeQuery();
+
+			while (resultSet.next()) {
+				countryList.add(new Country(resultSet.getString(2),
+						resultSet.getString(3)));
+			}
+			statement.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			closeConnection();
+		}
+		return countryList;
+	}
+
+	public List<Country> getCountryListStartWith(String name) {
+		List<Country> countryList = new ArrayList<>();
+
+		try {
+			Connection connection = openConnection();
+			PreparedStatement statement = connection
+					.prepareStatement(GET_COUNTRIES_BY_NAME_SQL);
+			statement.setString(1, name + "%");
+			ResultSet resultSet = statement.executeQuery();
+
+			while (resultSet.next()) {
+				countryList.add(new Country(resultSet.getString(2),
+						resultSet.getString(3)));
+			}
+			statement.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			closeConnection();
+		}
+		return countryList;
+	}
+}
