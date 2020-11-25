8e7cc673debe530e2b55a1bc0b059d59cf3d25d0
ch <mkmsacc@outlook.com>
2020-11-25 15:08:52 +0700

    Update jdbc to spring

--- src/main/java/jdbc/CountryDao.java
+++ src/main/java/jdbc/CountryDao.java
@@ -1,60 +1,31 @@
 package jdbc;
 
-import static jdbc.ConnectionManager.*;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
-public class CountryDao {
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+public class CountryDao extends JdbcDaoSupport {
 	private static final String GET_ALL_COUNTRIES_SQL = "select * from country";
-	private static final String GET_COUNTRIES_BY_NAME_SQL = "select * from country where name like ?";
+	private static final String GET_COUNTRIES_BY_NAME_SQL = "select * from country where name like :name";
+
+	private static final CountryRowMapper COUNTRY_ROW_MAPPER = new CountryRowMapper();
 
 	public List<Country> getCountryList() {
-		List<Country> countryList = new ArrayList<>();
-
-		try {
-			Connection connection = openConnection();
-			PreparedStatement statement = connection
-					.prepareStatement(GET_ALL_COUNTRIES_SQL);
-			ResultSet resultSet = statement.executeQuery();
-
-			while (resultSet.next()) {
-				countryList.add(new Country(resultSet.getString(2),
-						resultSet.getString(3)));
-			}
-			statement.close();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			closeConnection();
-		}
+		List<Country> countryList = getJdbcTemplate()
+				.query(GET_ALL_COUNTRIES_SQL, COUNTRY_ROW_MAPPER);
 		return countryList;
 	}
 
 	public List<Country> getCountryListStartWith(String name) {
-		List<Country> countryList = new ArrayList<>();
-
-		try {
-			Connection connection = openConnection();
-			PreparedStatement statement = connection
-					.prepareStatement(GET_COUNTRIES_BY_NAME_SQL);
-			statement.setString(1, name + "%");
-			ResultSet resultSet = statement.executeQuery();
-
-			while (resultSet.next()) {
-				countryList.add(new Country(resultSet.getString(2),
-						resultSet.getString(3)));
-			}
-			statement.close();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			closeConnection();
-		}
-		return countryList;
+		NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(
+				getDataSource());
+		SqlParameterSource sqlParameterSource = new MapSqlParameterSource(
+				"name", name + "%");
+		return namedParameterJdbcTemplate.query(GET_COUNTRIES_BY_NAME_SQL,
+				sqlParameterSource, COUNTRY_ROW_MAPPER);
 	}
+
 }
